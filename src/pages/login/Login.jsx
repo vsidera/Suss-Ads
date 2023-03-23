5b990c2569ef9b02cc427b00867142eebd1443f7
vsidera <siderravictor@gmail.com>
2023-03-23 14:12:44 +0300

    style home page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -72,19 +72,23 @@ const Login = () => {
     });
   };
 
+  const logoUrl = `${process.env.PUBLIC_URL}/images/logo.png`;
+
+
   return (
     <section class="bg-gray-50 dark:bg-gray-900">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Suss Advertising
-            </h1>
+          <div class="flex items-center justify-center mb-4">
+          <img src={logoUrl} alt="Logo" />
+            </div>
+     
             <form class="space-y-4 md:space-y-6" action="#">
               <div>
                 <label
                   for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  class="block mb-2 text-lg font-thin text-black dark:text-white text-center"
                 >
                   Your email
                 </label>
@@ -102,7 +106,7 @@ const Login = () => {
               <div>
                 <label
                   for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  class="block mb-2 text-lg font-thin text-gray-900 dark:text-white text-center"
                 >
                   Password
                 </label>
@@ -139,7 +143,7 @@ const Login = () => {
                 </div>
                 <a
                   href="#"
-                  class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  class="text-sm font-thin hover:underline dark:text-primary-500"
                 >
                   Forgot password?
                 </a>
@@ -147,7 +151,7 @@ const Login = () => {
               <button
                 onClick={handleLogin}
                 type="submit"
-                class="w-full text-gray-900 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                class="w-full text-white bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Sign in
               </button>
